d296fffc18d7c9079fa13a8ef6ea53671e9b8bf8
kanonieer <kanonieer@o2.pl>
2016-06-23 20:54:19 +0200

    dodanie kartki na dzien taty

---
+++ script/card.js
@@ -0,0 +1,8 @@
+var main = function(){
+    $(".card").slideDown(6000);
+    $('.greetings1').fadeIn(4000);
+    $('.greetings2').fadeIn(5000);
+    $('.greetings3').fadeIn(6000);
+  
+};
+$(document).ready(main);
